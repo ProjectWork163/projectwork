b40252005540e65e205b0f68a2021335b814573f
Mckay163 <heroes1993@yandex.ru>
2014-06-18 11:25:32 +0300

    Work3

--- JavaApplication1/src/javaapplication1/JavaApplication1.java
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -8,6 +8,7 @@ public class JavaApplication1 {
     public static void main(String[] args) {
         
         System.out.println("Hello");
+        System.out.println("Hi" + ""+ "!");
     }
     
 }
