278cbc4f30cc10fe6c26619c32795bc5b8dfbe54
Mckay163 <heroes1993@yandex.ru>
2014-06-18 10:43:22 +0300

    New

--- JavaApplication1/src/javaapplication1/JavaApplication1.java
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -16,7 +16,6 @@ public class JavaApplication1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
+      //the end
     
 }
