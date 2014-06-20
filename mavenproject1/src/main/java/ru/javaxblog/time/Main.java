2eac627768455b76d353e8fc42c15b9e3607098d
Mckay163 <heroes1993@yandex.ru>
2014-06-20 11:33:18 +0400

    new

---
+++ mavenproject1/src/main/java/ru/javaxblog/time/Main.java
@@ -0,0 +1,10 @@
+package ru.javaxblog.time;
+
+import javax.swing.JOptionPane;
+
+public class Main {
+
+    public static void main(String []args){
+        System.out.print("Hello111222333");
+    }
+}
